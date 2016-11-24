00df58e2f756c33f14a1ea533b5aac45fd8706d0
Paulo McNally <paulomcnally@gmail.com>
2016-11-24 13:47:02 -0600

    implement notifications

---
+++ EnEmergencias/app/src/main/java/com/mc/emergency/services/InstanceIdService.java
@@ -0,0 +1,14 @@
+package com.mc.emergency.services;
+
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class InstanceIdService extends FirebaseInstanceIdService {
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+    }
+}
---
+++ EnEmergencias/app/src/main/java/com/mc/emergency/services/MessagingService.java
@@ -0,0 +1,12 @@
+package com.mc.emergency.services;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // TODO(developer): Handle FCM messages here.
+    }
+}
